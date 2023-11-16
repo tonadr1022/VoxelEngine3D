bc593c3f724f520ddfd7f4b413079bc6241570c6
Tony Adriansen <tony.adriansen@gmail.com>
2023-11-15 22:15:14 -0600

    first commit

---
+++ src/Application.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Tony Adriansen on 11/15/23.
+//
+
+#include "Application.h"
---
+++ src/Application.h
@@ -0,0 +1,14 @@
+//
+// Created by Tony Adriansen on 11/15/23.
+//
+
+#ifndef VOXEL_ENGINE_APPLICATION_H
+#define VOXEL_ENGINE_APPLICATION_H
+
+
+class Application {
+
+};
+
+
+#endif //VOXEL_ENGINE_APPLICATION_H
