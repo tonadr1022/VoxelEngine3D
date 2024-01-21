d833618b27baff104871d8bca36c82c5b757dc02
Tony Adriansen <tony.adriansen@gmail.com>
2024-01-21 08:17:04 -0600

    bug fix

--- src/resources/ResourceManager.cpp
+++ src/resources/ResourceManager.cpp
@@ -83,9 +83,9 @@ void ResourceManager::makeTexture2dArray(std::vector<Image> &images,
 
   // configure sampler
   // s = x-axis
-  glTexParameteri(GL_TEXTURE_2D_ARRAY, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);
+  glTexParameteri(GL_TEXTURE_2D_ARRAY, GL_TEXTURE_WRAP_S, GL_REPEAT);
   // t = y-axis
-  glTexParameteri(GL_TEXTURE_2D_ARRAY, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
+  glTexParameteri(GL_TEXTURE_2D_ARRAY, GL_TEXTURE_WRAP_T, GL_REPEAT);
   glTexParameteri(GL_TEXTURE_2D_ARRAY, GL_TEXTURE_MIN_FILTER, GL_NEAREST_MIPMAP_LINEAR);
   glTexParameteri(GL_TEXTURE_2D_ARRAY, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 
